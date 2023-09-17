b195837a2315c24713301883e09a35df6b2b43c1
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2023-09-17 13:17:05 -0700

    Added Feign clients, processor

--- infrastructure/src/main/kotlin/com/hexagonalArchitecture/feign/address/AddressFeignClient.kt
+++ infrastructure/src/main/kotlin/com/hexagonalArchitecture/feign/address/AddressFeignClient.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @FeignClient(
     name = "address",
-    url = "https://fakerapi.it/"
+    url = "\${feign.address.url}"
 )
 interface AddressFeignClient {
 
--- infrastructure/src/main/kotlin/com/hexagonalArchitecture/feign/company/CompanyFeignClient.kt
+++ infrastructure/src/main/kotlin/com/hexagonalArchitecture/feign/company/CompanyFeignClient.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @FeignClient(
     name = "company",
-    url = "https://fakerapi.it/"
+    url = "\${feign.company.url}"
 )
 interface CompanyFeignClient {
 
